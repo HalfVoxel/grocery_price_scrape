10e688ea4010766b5c1925643db7d86855a8af67
Aron Granberg <aron.granberg@gmail.com>
2020-09-29 19:16:24 +0200

    Add more stores

--- src/main.py
+++ src/main.py
@@ -10,13 +10,22 @@ import time
 from datetime import datetime
 from typing import List, Optional
 from itertools import zip_longest
+from collections import namedtuple
 
-store = "01143"
-store_long = "ica-kvantum-vallentuna-id_01143"
+Store = namedtuple("Store", ["short_id", "long_id"])
+
+stores = [
+    Store(short_id="01143", long_id="ica-kvantum-vallentuna-id_01143"),
+    Store(short_id="14926", long_id="ica-supermarket-faltoversten-id_14926"),
+    Store(short_id="13713", long_id="ica-kvantum-avesta-id_13713"),
+    Store(short_id="14992", long_id="ica-supermarket-sjostaden-id_14992")
+]
+
 
 def iter_chunks(iterable, n, fillvalue=None):
     args = [iter(iterable)] * n
     return zip_longest(*args, fillvalue=fillvalue)
+
 
 def memoize_to_disk():
     def decorator(original_func):
@@ -50,35 +59,40 @@ def memoize_to_disk():
 
 
 # @memoize_to_disk()
-def read_categories():
-    r = requests.get(f"https://handla.ica.se/api/product-info/v1/store/{store}/category/catalog80002")
+def read_categories(store: Store):
+    r = requests.get(f"https://handla.ica.se/api/product-info/v1/store/{store.short_id}/category/catalog80002")
     assert r.ok
 
     return r.json()
 
 # @memoize_to_disk()
-def list_category(category: str):
-    r = requests.get(f"https://handla.ica.se/api/content/v1/collections/facets/customer-type/B2C/store/{store_long}/products?categories={category}&bb=true")
-                    #    https://handla.ica.se/api/content/v1/collections/facets/customer-type/B2C/store/01143/products?categories=%22ica-online-catalog-id_catalog80002&bb=true
+
+
+def list_category(store: Store, category: str):
+    r = requests.get(
+        f"https://handla.ica.se/api/content/v1/collections/facets/customer-type/B2C/store/{store.long_id}/products?categories={category}&bb=true")
+    #    https://handla.ica.se/api/content/v1/collections/facets/customer-type/B2C/store/01143/products?categories=%22ica-online-catalog-id_catalog80002&bb=true
     assert r.ok
 
     return r.json()
 
 # @memoize_to_disk()
-def get_product_data(product_ids: List[int]):
+
+
+def get_product_data(store: Store, product_ids: List[int]):
     if len(product_ids) == 0:
         return []
 
     time.sleep(0.5)
-    url = f"https://handla.ica.se/api/content/v1/collection/customer-type/B2C/store/{store_long}/products-data?skus={','.join(product_ids)}"
+    url = f"https://handla.ica.se/api/content/v1/collection/customer-type/B2C/store/{store.long_id}/products-data?skus={','.join(product_ids)}"
     r = requests.get(url)
     if not r.ok:
         if len(product_ids) > 1:
             print("Error when accessing product IDs, breaking up into smaller chunks")
             first_half = product_ids[:len(product_ids)//2]
             second_half = product_ids[len(product_ids)//2:]
-            d1 = get_product_data(first_half)
-            d2 = get_product_data(second_half)
+            d1 = get_product_data(store, first_half)
+            d2 = get_product_data(store, second_half)
             return [product for h in [d1, d2] for product in h]
         else:
             print("Error when accessing " + str(product_ids[0]) + " skipping")
@@ -90,46 +104,51 @@ def get_product_data(product_ids: List[int]):
     return r.json()
 
 # @memoize_to_disk()
-def get_product_data_chunked(product_ids: List[int]):
+
+
+def get_product_data_chunked(store: Store, product_ids: List[int]):
     all_products = []
     for chunk in progressbar.progressbar(list(iter_chunks(product_ids, 50)), redirect_stdout=True):
         chunk = [x for x in chunk if x is not None]
-        products = get_product_data(chunk)
+        products = get_product_data(store, chunk)
         for product in products:
             # print(json.dumps(product))
             all_products.append(product)
-        
+
         # print(len(all_products))
-    
+
     return all_products
 
-    
+
 def expect(v: bool):
     if not v:
         print("A a soft assert failed. Something may have failed.")
 
+
 def main():
-    root_category = read_categories()
+    for store in stores:
+        root_category = read_categories(store)
 
-    all_items = list_category(root_category["seoUrl"])["items"]
+        all_items = list_category(store, root_category["seoUrl"])["items"]
 
-    seen_items = set()
-    unique_items = []
-    for item in all_items:
-        if not item["id"] in seen_items:
-            assert item["id"] is not None
-            seen_items.add(item["id"])
-            unique_items.append(item)
+        seen_items = set()
+        unique_items = []
+        for item in all_items:
+            if not item["id"] in seen_items:
+                assert item["id"] is not None
+                seen_items.add(item["id"])
+                unique_items.append(item)
 
-    expect(len(all_items) > 10000)
+        expect(len(all_items) > 10000)
 
-    products = get_product_data_chunked([item["id"] for item in unique_items if item["type"] == "product"])
-    
-    today = datetime.today()
-    dirpath = f"data/{store}/{today.strftime('%Y-%m-%d')}"
-    os.makedirs(dirpath)
+        products = get_product_data_chunked(store, [item["id"] for item in unique_items if item["type"] == "product"])
 
-    with gzip.GzipFile(dirpath + "/products.json.pickle.gzip", 'wb') as file:
-        file.write(pickle.dumps(products))
+        today = datetime.today()
+        dirpath = f"data/{store.short_id}/{today.strftime('%Y-%m-%d')}"
+        os.makedirs(dirpath)
+
+        with gzip.GzipFile(dirpath + "/products.json.pickle.gzip", 'wb') as file:
+            file.write(pickle.dumps(products))
+
 
 main()
